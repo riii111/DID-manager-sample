2d1fdceaffed36cf877ab81734d04529f3c0093d
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2025-02-11 01:00:38 +0900

    X256 try_from

--- protocol/src/did/sidetree/payload.rs
+++ protocol/src/did/sidetree/payload.rs
@@ -2,6 +2,22 @@ use crate::keyring::jwk::Jwk;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ServiceEndpoint {
+    // renameの意図はおそらく : W3CのDID仕様に準拠している。また、将来名前が変化しても維持可。そして、外部IFとしての整合性を保証（これがあることで、外部IFであることも理解できる）。
+    #[serde(rename = "id")]
+    pub id: String,
+
+    #[serde(rename = "type")]
+    pub r#type: String,
+
+    #[serde(rename = "serviceEndpoint")]
+    pub service_endpoint: String,
+
+    #[serde(rename = "description")]
+    pub description: Option<String>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct PublicKeyJwk {
     pub kty: String,
     pub crv: String,
@@ -59,6 +75,38 @@ pub trait ToPublicKey<T: TryInto<Jwk>> {
         key_id: String,
         purpose: Vec<String>,
     ) -> Result<PublicKeyPayload, T::Error>;
+}
+
+// - impl<T> ... 構造体がジェネリクスTを使用しており、実装するメソッド内でもTを扱う場合はこの宣言が必要
+// - TryFromを実装すると、TryIntoも自動で実装される（Rustの仕様）。その結果、本トレイトも利用可能
+impl<T> ToPublicKey<T> for T
+where
+    T: TryInto<Jwk>,
+{
+    /// K256KeyPairやX25519KeyPairが持つ公開鍵を、DIDDocumentなどで利用されるJWK形式に変換する
+    fn to_public_key(
+        self,
+        key_type: String,
+        key_id: String,
+        purpose: Vec<String>,
+    ) -> Result<PublicKeyPayload, <T>::Error> {
+        let jwk: Jwk = self.try_into()?;
+        Ok(PublicKeyPayload {
+            id: key_id,
+            r#type: key_type,
+            jwk,
+            purpose,
+        })
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DidPatchDocument {
+    #[serde(rename = "public_keys")]
+    pub public_keys: Vec<PublicKeyPayload>,
+
+    #[serde(rename = "service_endpoints")]
+    pub service_endpoints: Vec<ServiceEndpoint>,
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
--- protocol/src/keyring/jwk.rs
+++ protocol/src/keyring/jwk.rs
@@ -1,8 +1,14 @@
 // JWK: Json Web Key. RFC7517で定義
 // https://tex2e.github.io/rfc-translater/html/rfc7517.html
+use std::convert::{From, Into, TryFrom, TryInto};
 
+pub use data_encoding;
+use data_encoding::{DecodeError, DecodePartial, BASE64URL_NOPAD, BASE64_NOPAD};
+use k256::elliptic_curve::sec1::ToEncodedPoint;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
+/// DID DocumentやSidetreeペイロードで利用される形式
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Jwk {
     #[serde(rename = "kty")] // key type. example: "EC", "OKP", "RSA"...
@@ -18,3 +24,73 @@ pub struct Jwk {
     // 楕円曲線上のy座標（kty="EC"かつ曲線次第では省略可）
     y: Option<String>,
 }
+
+#[derive(Error, Debug)]
+pub enum JwkToK256Error {
+    #[error("missing y")]
+    MissingY,
+    #[error("decode error")]
+    Decode(DecodePartial),
+    #[error("different crv")]
+    DifferentCrv,
+    #[error("crypt error: {0}")]
+    Crypt(#[from] k256::elliptic_curve::Error),
+}
+
+#[derive(Error, Debug)]
+pub enum K256ToJwkError {
+    #[error("points are invalid")]
+    PointsInvalid,
+}
+
+fn decode_base64url(
+    s: &str,
+) -> Result<k256::elliptic_curve::FieldBytes<k256::Secp256k1>, JwkToK256Error> {
+    let mut result = k256::elliptic_curve::FieldBytes::<k256::Secp256k1>::default();
+    BASE64_NOPAD
+        .decode_mut(s.as_bytes(), &mut result)
+        .map_err(JwkToK256Error::Decode)?;
+    Ok(result)
+}
+
+/// JWK から k256::PublicKey への変換処理
+/// TryFromを実装することで、自動的にTryIntoも利用可能となる（Rustの仕様）
+impl TryFrom<Jwk> for k256::PublicKey {
+    type Error = JwkToK256Error;
+    fn try_from(value: Jwk) -> Result<Self, Self::Error> {
+        if value.crv != "secp256k1" {
+            return Err(JwkToK256Error::DifferentCrv);
+        }
+        if let Some(y) = value.y {
+            // Base64URLエンコードされた座標をエンコード
+            let x = decode_base64url(&value.x)?;
+            let y = decode_base64url(&y)?;
+
+            // 座標から公開鍵を構築
+            let pk = k256::EncodedPoint::from_affine_coordinates(&x, &y, false);
+            let pk = k256::PublicKey::from_sec1_bytes(pk.as_bytes())?;
+            Ok(pk)
+        } else {
+            Err(JwkToK256Error::MissingY)
+        }
+    }
+}
+/// k256::PublicKey から JWK への変換処理
+impl TryFrom<k256::PublicKey> for Jwk {
+    type Error = K256ToJwkError;
+    fn try_from(value: k256::PublicKey) -> Result<Self, Self::Error> {
+        let value = value.to_encoded_point(false);
+        let kty = "EC".to_string();
+        let crv = "secp256k1".to_string();
+        match value.coordinates() {
+            k256::elliptic_curve::sec1::Coordinates::Uncompressed { x, y } => {
+                let x = BASE64URL_NOPAD.encode(x);
+                let y = Some(BASE64URL_NOPAD.encode(y));
+                Ok(Jwk { kty, crv, x, y })
+            }
+            _ => Err(K256ToJwkError::PointsInvalid),
+        }
+    }
+}
+
+// TODO: テストコード
