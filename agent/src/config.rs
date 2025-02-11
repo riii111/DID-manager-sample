de8d94a6ceace7af2e1d87a937c26a340e6b9f13
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2025-02-11 16:44:24 +0900

    unused field

--- agent/src/config.rs
+++ agent/src/config.rs
@@ -184,6 +184,7 @@ impl SingletonAppConfig {
     }
 }
 
+#[allow(static_mut_refs)]
 pub fn app_config() -> Box<SingletonAppConfig> {
     static mut SINGLETON: Option<Box<SingletonAppConfig>> = None;
     static ONCE: Once = Once::new();
@@ -205,7 +206,7 @@ pub fn app_config() -> Box<SingletonAppConfig> {
 #[derive(Debug)]
 pub struct ServerConfig {
     did_http_endpoint: String,
-    did_attachment_link: String,
+    // did_attachment_link: String,
 }
 
 impl Default for ServerConfig {
@@ -218,21 +219,21 @@ impl ServerConfig {
     pub fn new() -> ServerConfig {
         let did_endpoint =
             env::var("MIAX_DID_HTTP_ENDPOINT").unwrap_or("https://did.miacross.io".to_string());
-        let link =
-            env::var("MIAX_DID_ATTACHMENT_LINK").unwrap_or("https://did.miacross.io".to_string());
+        // let link =
+        // env::var("MIAX_DID_ATTACHMENT_LINK").unwrap_or("https://did.miacross.io".to_string());
 
         ServerConfig {
             did_http_endpoint: did_endpoint,
-            did_attachment_link: link,
+            // did_attachment_link: link,
         }
     }
 
     pub fn did_http_endpoint(&self) -> String {
         self.did_http_endpoint.clone()
     }
-    pub fn did_attachment_link(&self) -> String {
-        self.did_attachment_link.clone()
-    }
+    // pub fn did_attachment_link(&self) -> String {
+    // self.did_attachment_link.clone()
+    // }
 }
 
 pub fn server_config() -> ServerConfig {
