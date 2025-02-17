ef06d3d55a165ba039112ffab1e51d586b22f3ff
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2025-02-18 00:52:46 +0900

    Update did_vc.rs

--- protocol/src/verifiable_credentials/did_vc.rs
+++ protocol/src/verifiable_credentials/did_vc.rs
@@ -1,5 +1,8 @@
+use crate::did::did_repository::GetPublicKeyError;
 use crate::keyring::keypair;
+use crate::verifiable_credentials::credential_signer::CredentialSignerVerifyError;
 use crate::verifiable_credentials::types::VerifiableCredentials;
+use thiserror::Error;
 
 #[trait_variant::make(Send)]
 pub trait DidVcService: Sync {
@@ -10,4 +13,20 @@ pub trait DidVcService: Sync {
         model: VerifiableCredentials,
         from_keyring: &keypair::KeyPairing,
     ) -> Result<VerifiableCredentials, Self::GenerateError>;
+    async fn verify(
+        &self,
+        model: VerifiableCredentials,
+    ) -> Result<VerifiableCredentials, Self::VerifyError>;
+}
+
+#[derive(Debug, Error)]
+pub enum DidVcServiceVerifyError<FindIdentifierError: std::error::Error> {
+    #[error("did public key not found. did: {0}")]
+    PublicKeyNotFound(#[from] GetPublicKeyError),
+    #[error("failed to get did document: {0}")]
+    DidDocNotFound(String),
+    #[error("failed to find identifier: {0}")]
+    FindIdentifier(FindIdentifierError),
+    #[error("credential signer error")]
+    VerifyFailed(#[from] CredentialSignerVerifyError),
 }
