99c132ebf8c2e1ef1b7f9742cd690c80ded0be62
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2025-02-17 12:09:27 +0900

    fix error and format

--- agent/src/network.rs
+++ agent/src/network.rs
@@ -1,9 +1,9 @@
-use std::fs::OpenOptions;
-use std::sync::{Arc, Mutex, MutexGuard, Once};
-use std::path::Path;
 use home_config::HomeConfig;
-use serde::{Deserialize, Serialize};
 use io;
+use serde::{Deserialize, Serialize};
+use std::fs::OpenOptions;
+use std::path::Path;
+use std::sync::{Arc, Mutex, MutexGuard, Once};
 
 #[derive(Clone)]
 pub struct SingletonNetworkConfig {
@@ -17,20 +17,20 @@ impl SingletonNetworkConfig {
 }
 
 pub fn network_config() -> Box<SingletonNetworkConfig> {
-  static mut SINGLETON: Option<Box<SingletonNetworkConfig>> = None:
-  static ONCE: Once = Once::new();
+    static mut SINGLETON: Option<Box<SingletonNetworkConfig>> = None;
+    static ONCE: Once = Once::new();
 
-  unsafe {
-    ONCE.call_once(|| {
-      let singleton = SingletonNetworkConfig {
-        inner: Arc::new(Mutex::new(Network::new())),
-      };
+    unsafe {
+        ONCE.call_once(|| {
+            let singleton = SingletonNetworkConfig {
+                inner: Arc::new(Mutex::new(Network::new())),
+            };
 
-      SINGLETON = Some(Box::new(singleton))
-    });
+            SINGLETON = Some(Box::new(singleton))
+        });
 
-    SINGLETON.clone().unwrap()
-  }
+        SINGLETON.clone().unwrap()
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -51,28 +51,28 @@ pub struct Network {
 }
 
 impl Network {
-  fn touch(path: &Path) -> io::Result<()> {
-    let mut file = OpenOptions::new()
-        .truncate(true)
-        .create(true)
-        .write(true)
-        .open(path)?;
-    file.write_all(b"{}")?;
-    Ok(())
-  }
-  const APP_NAME: &'static str = "miax";
-  const CONFIG_FILE: &'static str = "network.json";
-
-  fn new() -> Self {
-    let config = HomeConfig::with_config_dir(Network::APP_NAME, Network::CONFIG_FILE);
-    let config_dir = config.path().parent().expect("unreachable");
-
-    if !Path::exists(config.path()) {
-      fs::create_dir_all(config_dir).unwrap();  // TODO: unwrap_log
-      Self::touch(config.path()).unwrap();  //TODO: unwrap_log
+    fn touch(path: &Path) -> io::Result<()> {
+        let mut file = OpenOptions::new()
+            .truncate(true)
+            .create(true)
+            .write(true)
+            .open(path)?;
+        file.write_all(b"{}")?;
+        Ok(())
     }
-    let root = config.json::<ConfigNetwork>().unwrap(); //TODO: unwrap_log
+    const APP_NAME: &'static str = "miax";
+    const CONFIG_FILE: &'static str = "network.json";
 
-    Network { config, root }
-  }
+    fn new() -> Self {
+        let config = HomeConfig::with_config_dir(Network::APP_NAME, Network::CONFIG_FILE);
+        let config_dir = config.path().parent().expect("unreachable");
+
+        if !Path::exists(config.path()) {
+            fs::create_dir_all(config_dir).unwrap(); // TODO: unwrap_log
+            Self::touch(config.path()).unwrap(); //TODO: unwrap_log
+        }
+        let root = config.json::<ConfigNetwork>().unwrap(); //TODO: unwrap_log
+
+        Network { config, root }
+    }
 }
