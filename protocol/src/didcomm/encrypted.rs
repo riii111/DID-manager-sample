da63b9044f8b7e3863aaa42a0ff95f21c9899c8c
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2025-02-18 12:18:27 +0900

    added impl

--- protocol/src/didcomm/encrypted.rs
+++ protocol/src/didcomm/encrypted.rs
@@ -8,3 +8,15 @@ where
     vc_service: R,
     attachment_link: String,
 }
+
+impl<R> DidCommServiceWithAttachment<R>
+where
+    R: DidRepository + DidVcService,
+{
+    pub fn new(did_repository: R, attachment_link: String) -> Self {
+        Self {
+            vc_service: did_repository,
+            attachment_link,
+        }
+    }
+}
