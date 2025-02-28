593d5a323a446e64fd900442f791786b2d9bb404
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2025-03-01 01:55:17 +0900

    draft studio impl

--- agent/src/miax/utils/studio_client.rs
+++ agent/src/miax/utils/studio_client.rs
@@ -1,12 +1,37 @@
 use super::did_accessor::DidAccessorImpl;
 use crate::miax::utils::sidetree_client::SideTreeClient;
+use crate::server_config;
 use protocol::did::did_repository::DidRepositoryImpl;
 use protocol::didcomm::encrypted::DidCommServiceWithAttachment;
 use reqwest::Url;
+
+pub struct StudioClientConfig {
+    pub base_url: String,
+}
 
 pub struct StudioClient {
     pub base_url: Url,
     pub instance: reqwest::Client,
     pub didcomm_service: DidCommServiceWithAttachment<DidRepositoryImpl<SideTreeClient>>,
     pub did_accessor: DidAccessorImpl,
+}
+
+impl StudioClient {
+    pub fn new(_config: &StudioClientConfig) -> anyhow::Result<Self> {
+        let url = Url::parse(&_config.base_url.to_string())?;
+        let client = reqwest::Client::new();
+        let server_config = server_config();
+        let sidetree_client = SideTreeClient::new(&server_config.did_http_endpoint());
+        let did_repository = DidRepositoryImpl::new(sidetree_client);
+        let didcomm_service =
+            DidCommServiceWithAttachment::new(did_repository, server_config.did_attachment_link());
+        let did_accessor = DidAccessorImpl {};
+
+        Ok(StudioClient {
+            instance: client,
+            base_url: url,
+            didcomm_service,
+            did_accessor,
+        })
+    }
 }
--- agent/src/services/studio.rs
+++ agent/src/services/studio.rs
@@ -1,10 +1,42 @@
-use crate::miax::utils::did_accessor::DidAccessorImpl;
-use crate::miax::utils::sidetree_client::SideTreeClient;
-use crate::miax::utils::studio_client::StudioClient;
-use protocol::did::did_repository::DidRepositoryImpl;
+use crate::config::server_config;
+use crate::miax::utils::did_accessor::{self, DidAccessorImpl};
+use crate::miax::utils::sidetree_client::{self, SideTreeClient};
+use crate::miax::utils::studio_client::{StudioClient, StudioClientConfig};
+use protocol::did::did_repository::{self, DidRepositoryImpl};
+use serde::Deserialize;
 
 pub struct Studio {
     http_client: StudioClient,
     did_repository: DidRepositoryImpl<SideTreeClient>,
     did_accessor: DidAccessorImpl,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct NetworkResponse {
+    pub secret_key: String,
+    pub project_did: String,
+    pub recipient_dids: Vec<String>,
+    pub studio_endpoint: String,
+    pub heartbeat: u64,
+}
+
+impl Studio {
+    pub fn new() -> Self {
+        let server_config = server_config();
+        let client_config: StudioClientConfig = StudioClientConfig {
+            base_url: server_config.studio_http_endpoint(),
+        };
+        let client = match StudioClient::new(&client_config) {
+            Ok(v) => v,
+            Err(e) => {
+                log::error!("{:?}", e);
+                panic!()
+            }
+        };
+
+        let sidetree_client = SideTreeClient::new(&server_config.did_http_endpoint())
+            .expect("failed to create sidetree client");
+        let did_repository = DidRepositoryImpl::new(sidetree_client);
+        let did_accessor = DidAccessorImpl {};
+    }
 }
