609671bb72bc3080076b684a71de98808aa3e36d
riii111 <ryo.ichinose@collabogate.com>
2025-03-22 10:57:06 +0900

    feat. polling_task

--- agent/src/controllers/public/miax_receive.rs
+++ agent/src/controllers/public/miax_receive.rs
@@ -1,4 +1,7 @@
+use std::time::Duration;
+
 use crate::services::{miax::MiaX, studio::Studio};
+use tokio_util::sync::CancellationToken;
 
 struct MessageReceiveUsecase {
     studio: Studio,
@@ -27,4 +30,27 @@ impl MessageReceiveUsecase {
     pub async fn receive_message(&self) -> anyhow::Result<()> {
         unimplemented!("receive message")
     }
+}
+
+pub async fn polling_task(shutdown_token: CancellationToken) {
+    log::info!("Polling task is started");
+
+    let usecase = MessageReceiveUsecase::new();
+
+    let mut interval = tokio::time::interval(Duration::from_secs(3600));
+    loop {
+        tokio::select! {
+            _ = interval.tick() => {
+                match usecase.receive_message().await {
+                    Ok(_) => {},
+                    Err(e) => log::error!("Error: {:?}", e),
+                }
+            }
+            _ = shutdown_token.cancelled() => {
+                break;
+            }
+        }
+    }
+
+    log::info!("Polling task is stopped")
 }
