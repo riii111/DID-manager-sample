ddaab06b580943735747ea5d6b8ff855a89cfdcb
riii111 <ryo.ichinose@collabogate.com>
2025-03-23 01:58:36 +0900

    added process

---
+++ controller/src/validator/process.rs
@@ -0,0 +1,13 @@
+#[cfg(unix)]
+use nix::sys::signal;
+#[cfg(unix)]
+use nix::unistd::Pid;
+use std::env;
+
+pub fn is_manage_by_systemd() -> bool {
+    env::var("INVOCATION_ID").is_ok()
+}
+
+pub fn is_manage_socket_activation() -> bool {
+    env::var("LISTEN_PID").is_ok() && env::var("LISTEN_FDS").is_ok()
+}
